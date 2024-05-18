6a9799967f96788a2cd267c6b3a2d8e0ab3074bf
davideferri2006@gmail.com <davideferri2006@gmail.com>
2024-05-19 00:06:56 +0200

    modifiche riguardanti soprattutto la HOME

--- progetto_project_day_2024/src/pages/Home/Home.jsx
+++ progetto_project_day_2024/src/pages/Home/Home.jsx
@@ -4,10 +4,23 @@ import '../Home/Home.css'
 export const Home = () => {
   return (
     <home id="home">
-    <div>
+    <div id='div'>
         <img id="robot" src='https://i.pinimg.com/originals/35/c5/0a/35c50a9f3b1d5dcefec93b7ccbb57f90.jpg'
         ></img>
+      
+
+        <p id="descrizione"> 
+
+          Qua ci sarà la descrizione ? Qualcosa di simile che spieghi in breve il tutto,
+          oppure altro che comunque servirà ad impatto!!! Importante, impatto. Qualcosa che 
+          trasmetta subito e in breve cosa ci sarà e cosa farà ? Boh, ma impatto.
+
+        </p>
+
     </div>
+  
+     
+    
     </home>
   )
 }
