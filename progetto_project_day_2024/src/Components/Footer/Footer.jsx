3baa0082d0a2885bab0eff3d6c037519357251f4
davideferri2006@gmail.com <davideferri2006@gmail.com>
2024-05-20 15:47:42 +0200

    home conclusa, manca solo descrizione che è solamente da sostituire nel testo, e da rivedere magari il footer.

--- progetto_project_day_2024/src/Components/Footer/Footer.jsx
+++ progetto_project_day_2024/src/Components/Footer/Footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import "../Footer/Footer.css"
+import { Link } from 'react-router-dom';
+
 export const Footer = () => {
   return (
-    <footer id='footer'>
-    <div>Footer</div>
-    </footer>
+    <footer class="footer">
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-logo">
+                <img src="https://img.freepik.com/premium-photo/cyber-digital-world-android-robot-pointing-finger-computer-coding-ai-chatbot-background_629685-1221.jpg" alt="Logo"></img>
+            </div>
+            <div class="footer-links">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Servizi</a></li>
+                    <li><Link to="/About_us">Contatti</Link></li>
+                </ul>
+            </div>
+            
+        </div>
+        <div class="footer-info">
+            <p>© 2024 ITIS Archimede</p>
+            <p>Indirizzo: Via Greggia, 52, 24047 Treviglio BG</p>
+            <p>Email: ferri.davide.stu@isarchimede.it | barboni.luca.stu@isarchimede.it |   carlino.mattia.stu@isarchimede.it
+            </p>
+        </div>
+    </div>
+</footer>
+
   )
 }
 
