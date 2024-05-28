874d23e32e025c41fc981cf1b8c9e952695e6088
Mattia Carlino <mattia.carlino.mc06@gmail.com>
2024-05-28 13:08:15 +0200

    cose a caso

--- progetto_project_day_2024/src/pages/Homepage_account/Homepage_account.jsx
+++ progetto_project_day_2024/src/pages/Homepage_account/Homepage_account.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import '../Homepage_account/Homepage.css'
+import { useState } from 'react';
 import Istitutes from '../../istitute.json'
-import { Option_selction_address } from '../../Components/Account_insertion/Option_selection_address';
-import { Option_selction_istitute } from '../../Components/Account_insertion/Option_selction_istitute';
 
 export const Homepage_account = () => {
   return (
@@ -11,22 +10,84 @@ export const Homepage_account = () => {
       <div className='center-space'>
         <div className='field'>
           <form >
-            <div className='name-field'>
-              <label> Inserisci Nome </label>
-              <input type='text' class= "form-control" placeholder='Nome'></input>
+            <div className='field' class="form-floating mb-3">
+              <input type='text' class="form-control" id="floatingInput" placeholder='Nome'></input>
+              <label for="floatingInput"> Inserisci Nome </label>
             </div>
-            <div className='surname-field'>
-              <label> Inserisci Cognome </label>
-              <input type='text' class= "form-control" placeholder='Cognome'></input>
+            <div className='field' class="form-floating mb-3">
+              <input type='text' class="form-control" id="floatingInput" placeholder='Cognome'></input>
+              <label for="floatingInput"> Inserisci Cognome </label>
             </div>
-            <div className='istitute-field'>
+            <div className='field'>
               <label>Scuola</label>
               <select class="form-select" name='istitute-field'>
-                <option selected >Seleziona la tua scuola</option>  
-                <option></option>
-              </select>
+                  <option value='null' selected>Seleziona la tua scuola</option>
+                  <hr />
+                  <option value="liceo classico">Liceo Classico</option>
+                  <hr />
+                  <option value="liceo linguistico">Liceo Linguistico</option>
+                  <hr />
+                  <optgroup label='Liceo Scientifico'>
+                    <option value="liceo scientifico">Liceo Scientifico tradizionale</option>
+                    <option value="liceo scientifico, scienze applicate">Liceo Scientifico scienze applicate</option>
+                    <option value="liceo scientifico, sezione sportiva">Liceo Scientifico sportivo</option>
+                  </optgroup>
+                  <hr />
+                  <optgroup label='Liceo delle scienze umane'>
+                    <option value="liceo delle scienze umane">Liceo delle scienze umane</option>
+                    <option value="liceo delle scienze umane, economico sociale">Liceo delle scienze umane, economico sociale</option>
+                  </optgroup>
+                  <hr />
+                  <optgroup label='Liceo artistico'>
+                    <option value="liceo artistico, arti figurative">Indirizzo Arti Figurative</option>
+                    <option value="liceo artistico, architettura e ambiente">Indirizzo Architettura e Ambiente</option>
+                    <option value="liceo artistico, audiovisivo e multimediale">Indirizzo Audiovisivo e Multimediale</option>
+                    <option value="liceo artistico, design">Indirizzo Design</option>
+                    <option value="liceo artistico, grafica">Indirizzo Grafica</option>
+                    <option value="liceo artistico, scenografia">Indirizzo Scenografia</option>
+                  </optgroup>
+                  <hr />
+                  <optgroup label='Liceo musicale e coreutico'>
+                    <option value="liceo musicale e coreutico, musicale">Indirizzo Musicale</option>
+                    <option value="liceo musicale e coreutico, coreutico">Indirizzo Coreutico</option>
+                  </optgroup>
+                  <hr />
+                  <optgroup label='Istituto tecnico economico'>
+                    <option value="istituto tecnico economico, amministrazione, finanza e marketing">Amministrazione, Finanza e Marketing</option>
+                    <option value="istituto tecnico economico, sistemi informativi aziendali">Sistemi Informativi Aziendali</option>
+                    <option value="istituto tecnico economico, relazioni internazionali per il marketing">Relazioni Internazionali per il Marketing</option>
+                    <option value="istituto tecnico economico, turismo">Turismo</option>
+                  </optgroup>
+                  <hr />
+                  <optgroup label='Istituto tecnico tecnologico'>
+                    <option value="istituto tecnico tecnologico, meccanica, meccatronica ed energia">Meccanica, Meccatronica ed Energia</option>
+                    <option value="istituto tecnico tecnologico, trasporti e logistica">Trasporti e Logistica</option>
+                    <option value="istituto tecnico tecnologico, elettronica ed elettrotecnica">Elettronica ed Elettrotecnica</option>
+                    <option value="istituto tecnico tecnologico, informatica e telecomunicazioni">Informatica e Telecomunicazioni</option>
+                    <option value="istituto tecnico tecnologico, grafica e comunicazione">Grafica e Comunicazione</option>
+                    <option value="istituto tecnico tecnologico, chimica, materiali e biotecnologie">Chimica, Materiali e Biotecnologie</option>
+                    <option value="istituto tecnico tecnologico, sistema moda">Sistema Moda</option>
+                    <option value="istituto tecnico tecnologico, agraria, agroalimentare e agroindustria">Agraria, Agroalimentare e Agroindustria</option>
+                    <option value="istituto tecnico tecnologico, costruzioni, ambiente e territorio">Costruzioni, Ambiente e Territorio</option>
+                  </optgroup>
+                  <hr />
+                  <optgroup label='Istituto professionale per i servizi'>
+                    <option value="istituto professionale per i servizi, servizi per l'agricoltura, lo sviluppo rurale e la selvicoltura">Servizi per l'Agricoltura, lo Sviluppo Rurale e la Selvicoltura</option>
+                    <option value="istituto professionale per i servizi, servizi socio-sanitari">Servizi Socio-Sanitari</option>
+                    <option value="istituto professionale per i servizi, servizi per l'enogastronomia e l'ospitalità alberghiera">Servizi per l'Enogastronomia e l'Ospitalità Alberghiera</option>
+                    <option value="istituto professionale per i servizi, servizi commerciali">Servizi Commerciali</option>
+                    <option value="istituto professionale per i servizi, servizi per la sanità e l'assistenza sociale">Servizi per la Sanità e l'Assistenza Sociale</option>
+                  </optgroup>
+                  <hr />
+                  <optgroup label=" Istituto professionale per lindustria e l artigianato">
+                    <option value="istituto professionale per l'industria e l'artigianato, manutenzione e assistenza tecnica">Manutenzione e Assistenza Tecnica</option>
+                    <option value="istituto professionale per l'industria e l'artigianato, industria e artigianato per il made in italy">Industria e Artigianato per il Made in Italy</option>
+                    <option value="istituto professionale per l'industria e l'artigianato, arti ausiliarie delle professioni sanitarie: odontotecnico">Arti Ausiliarie delle Professioni Sanitarie: Odontotecnico</option>
+                    <option value="istituto professionale per l'industria e l'artigianato, arti ausiliarie delle professioni sanitarie: ottico">Arti Ausiliarie delle Professioni Sanitarie: Ottico</option>
+                  </optgroup>
+                </select>
             </div>
-            <div className='class-field'>
+            <div className='field'>
               <label>Scuola</label>
               <select class="form-select">
                 <option selected>Seleziona la tua classe</option>
